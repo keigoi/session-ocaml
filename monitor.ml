faf29f78f86c8ae776a26e167302324af225dae2
Keigo IMAI <keigo.imai@gmail.com>
2012-12-07 11:50:13 +0900

    abandon almost meaningless functors

--- monitor.ml
+++ monitor.ml
@@ -1,44 +1,46 @@
-module type Param = sig 
-  type 'a data
-  type 'a state
-  val create : 'a data -> 'a state
-  val destruct : 'a state -> 'a data
-end
+module M = Mutex
+module C = Condition
 
+type 'a t = ('a * M.t * C.t)
 
-module Make = functor (X:Param) -> struct
-  module M = Mutex
-  module C = Condition
+type 'a wait = WaitMore | Return of 'a
 
-  type 'a t = ('a X.state * M.t * C.t)
-  
-  type 'a wait = WaitMore | Return of 'a
-  
-  let create v = (X.create v, M.create (), C.create ())
+let create v = (v, M.create (), C.create ())
 
-  let wait (cell,m,c) (func: 'a X.state -> 'b wait) : 'b = 
-    Mutex.lock m;
-    let rec loop () =
-      begin try
-        match func cell with
-        | Return v -> v
-        | WaitMore -> begin
-            Condition.wait c m; 
-            loop ()
-          end
-      with e ->
-        Mutex.unlock m;
-        raise e
-      end
-    in
-    let v = loop () in
+let wait (cell,m,c) (func: 'a -> 'b wait) : 'b = 
+  Mutex.lock m;
+  let rec loop () =
+    begin try
+      match func cell with
+      | Return v -> v
+      | WaitMore -> begin
+          Condition.wait c m; 
+          loop ()
+        end
+    with e ->
+      Mutex.unlock m;
+      raise e
+    end
+  in
+  let v = loop () in
+  Mutex.unlock m;
+  v
+
+let signal (_,_,c) = Condition.signal c
+
+let lock (cell,m,_) (func: 'a -> 'b) : 'b =
+  Mutex.lock m;
+  begin try
+    let v = func cell in
     Mutex.unlock m;
     v
-  
-  let signal (_,_,c) = Condition.signal c
+  with e ->
+    Mutex.unlock m;
+    raise e
+  end
 
-  let lock (cell,m,_) (func: 'a X.state -> 'b) : 'b =
-    Mutex.lock m;
+let try_lock (cell,m,_) (func: 'a -> 'b) (iffail:'b) : 'b =
+  if Mutex.try_lock m then begin
     begin try
       let v = func cell in
       Mutex.unlock m;
@@ -47,18 +49,6 @@ module Make = functor (X:Param) -> struct
       Mutex.unlock m;
       raise e
     end
-  
-  let try_lock (cell,m,_) (func: 'a X.state -> 'b) (iffail:'b) : 'b =
-    if Mutex.try_lock m then begin
-      begin try
-        let v = func cell in
-        Mutex.unlock m;
-        v
-      with e ->
-        Mutex.unlock m;
-        raise e
-      end
-    end else iffail
-  
-  let get (cell,_,_) = X.destruct cell
-end
+  end else iffail
+
+let get (cell,_,_) = cell
--- monitor.mli
+++ monitor.mli
@@ -1,28 +1,9 @@
-(** Monitor in a functorial style. *)
-
-(**
-   The parameter type for the monitor. 
-   - 'a data is the raw representation type of the data that is hold inside the monitor, and
-   - 'a state is some kind of stateful data cell (typically 'a ref),
-     which is assured to be accessed by only one thread at one time.
- *)
-module type Param =
-  sig
-    type 'a data
-    type 'a state
-    val create : 'a data -> 'a state
-    val destruct : 'a state -> 'a data
-  end
-
-module Make :   
-  functor (X : Param) ->
-    sig
-      type 'a t
-      type 'a wait = WaitMore | Return of 'a
-      val create : 'a X.data -> 'a t
-      val signal : 'a t -> unit
-      val wait : 'a t -> ('a X.state -> 'b wait) -> 'b
-      val lock : 'a t -> ('a X.state -> 'b) -> 'b
-      val try_lock : 'a t -> ('a X.state -> 'b) -> 'b -> 'b
-      val get : 'a t -> 'a X.data
-    end
+(** Monitor *)
+type 'a t
+type 'b wait = WaitMore | Return of 'b
+val create : 'a -> 'a t
+val signal : 'a t -> unit
+val wait : 'a t -> ('a -> 'b wait) -> 'b
+val lock : 'a t -> ('a -> 'b) -> 'b
+val try_lock : 'a t -> ('a -> 'b) -> 'b -> 'b
+val get : 'a t -> 'a
