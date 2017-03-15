2a471b682103475b5a1786deaf86000d917b214d
Keigo IMAI <keigo.imai@gmail.com>
2017-03-15 10:51:51 +0900

    recover

--- lib/session.mli
+++ lib/session.mli
@@ -123,22 +123,22 @@ module SessionN : sig
 
 end
 
-(* type 'a parse_result = *)
-(*   [`Partial of (string option -> 'a parse_result) (\* `Partial f. invariant: (f None) may not return Partial  *\) *)
-(*   |`Done    of 'a * string *)
-(*   |`Fail of string] *)
+type 'a parse_result =
+  [`Partial of (string option -> 'a parse_result) (* `Partial f. invariant: (f None) may not return Partial  *)
+  |`Done    of 'a * string
+  |`Fail of string]
 
-(* module type Adapter = sig *)
-(*   type raw_chan *)
-(*   type 'p net = raw_chan -> (('p, serv) sess * all_empty, all_empty, unit) monad *)
-(*   val req : ('v -> string) -> 'p net -> [`msg of req * 'v * 'p] net *)
-(*   val resp : (string -> 'v parse_result) -> 'p net -> [`msg of resp * 'v * 'p] net *)
-(*   val sel : left:'p1 net -> right:'p2 net -> *)
-(*           [`branch of req * [`left of 'p1|`right of 'p2]] net *)
-(*   val bra : left:((string -> 'v1 parse_result) * 'p1 net) -> right:'p2 net -> *)
-(*           [`branch of resp * [`left of [`msg of resp * 'v1 * 'p1] |`right of 'p2]] net *)
-(*   val cls : [`close] net *)
-(* end *)
+module type Adapter = sig
+  type raw_chan
+  type 'p net = raw_chan -> (('p, serv) sess, empty, unit) monad
+  val req : ('v -> string) -> 'p net -> [`msg of req * 'v * 'p] net
+  val resp : (string -> 'v parse_result) -> 'p net -> [`msg of resp * 'v * 'p] net
+  val sel : left:'p1 net -> right:'p2 net ->
+          [`branch of req * [`left of 'p1|`right of 'p2]] net
+  val bra : left:((string -> 'v1 parse_result) * 'p1 net) -> right:'p2 net ->
+          [`branch of resp * [`left of [`msg of resp * 'v1 * 'p1] |`right of 'p2]] net
+  val cls : [`close] net
+end
 
 module Syntax : sig
   val (>>=) : ('x,'y,'a) monad -> ('a -> ('y, 'z, 'b) monad) -> ('x,'z,'b) monad
